1f40fc5b49f34800e804a006707da2f62743220f
Tom Bombadil <amanibhavam@destructuring.org>
2012-08-22 09:59:13 +0200

    prevent ubunu from caching eth0 address

--- vagrant.sh
+++ vagrant.sh
@@ -20,11 +20,6 @@ sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
 rm -f "$pth_guestadditions"
 
-# udev cleanup
+# prevent udev from caching eth0 MAC
 rm -rf /etc/udev/rules.d/70-persistent-net.rules
 mkdir -p /etc/udev/rules.d/70-persistent-net.rules
-rm -rf /dev/.udev/
-rm -rf /lib/udev/rules.d/75-persistent-net-generator.rules
-
-# dhcp cleanup
-rm -f /var/lib/dhcp3/*
