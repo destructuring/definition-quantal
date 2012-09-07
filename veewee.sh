d44453cd6072ed7f07a21ea0ef4600aa117111ae
Tom Bombadil <amanibhavam@destructuring.org>
2012-09-06 21:21:38 -0700

    need ntp since time keeps drifting

--- veewee.sh
+++ veewee.sh
@@ -8,6 +8,7 @@ export DEBIAN_FRONTEND="noninteractive"
 # update packages
 aptitude update
 aptitude install -q -y linux-server linux-headers-server linux-image-server linux-headers
+aptitude install -q -y ntp
 aptitude hold linux-server linux-headers-server linux-image-server linux-headers
 aptitude clean
 
