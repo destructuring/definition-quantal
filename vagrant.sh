a1a93d0fb13450a14839248a28a11a38efb8338e
Tom Bombadil <amanibhavam@destructuring.org>
2012-10-25 10:25:47 -0700

    let veewee transfer the guest additions to guest

--- vagrant.sh
+++ vagrant.sh
@@ -9,21 +9,17 @@ aptitude update
 aptitude safe-upgrade -q -y
 
 # vbox guest additions
-#ver_virtualbox="$(cat .vbox_version)"
-ver_virtualbox="4.1.22"
-url_guestadditions="http://download.virtualbox.org/virtualbox/$ver_virtualbox/VBoxGuestAdditions_$ver_virtualbox.iso"
+ver_virtualbox="$(cat .vbox_version)"
 pth_guestadditions="$HOME/VBoxGuestAdditions_$ver_virtualbox.iso"
-wget -nv -O "$pth_guestadditions" "$url_guestadditions"
 mount -o loop "$pth_guestadditions" /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
-rm -f "$pth_guestadditions"
-
-# wake up eth0
-perl -pe 'm{exit 0} && print "dhclient eth0\n"' -i /etc/rc.local
 
 # remove cached network configurations
 rm -f /etc/udev/rules.d/70-persistent-net.rules
 mkdir -p /etc/udev/rules.d/70-persistent-net.rules
 rm /lib/udev/rules.d/75-persistent-net-generator.rules
 rm -rf /dev/.udev/ /var/lib/dhcp3/*
+
+# wake up eth0
+perl -pe 'm{exit 0} && print "dhclient eth0\n"' -i /etc/rc.local
--- veewee.sh
+++ veewee.sh
@@ -7,8 +7,7 @@ export DEBIAN_FRONTEND="noninteractive"
 
 # update packages
 aptitude update
-aptitude install -q -y linux-server linux-headers-server linux-image-server linux-headers
-aptitude install -q -y ntp
+aptitude install -q -y linux-server linux-headers-server linux-image-server linux-headers ntp
 aptitude hold linux-server linux-headers-server linux-image-server linux-headers
 aptitude clean
 
