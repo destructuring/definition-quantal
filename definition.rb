a6350d8be2d5f26f8b27f39c25f049538179f291
Tom Bombadil <amanibhavam@destructuring.org>
2012-11-18 23:07:17 -0800

    move files into libexec, config, share

--- definition.rb
+++ definition.rb
@@ -1,2 +1,2 @@
 require 'yaml'
-Veewee::Session.declare(YAML.load(File.read(File.join(File.dirname(File.expand_path(__FILE__)), "definition.yml"))))
+Veewee::Session.declare(YAML.load(File.read(File.join(File.dirname(File.expand_path(__FILE__)), "config/definition.yml"))))
